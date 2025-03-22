52fa06799adb5e25dd9e46af06c849bd217564e9
Trisha Sharon <trishasharon11@gmail.com>
2025-03-22 20:08:00 +0530

    Reset--

---
+++ Mini Blog Application/DTO/Account/LoginDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mini_Blog_Application.DTO.Account
+{
+    public class LoginDto
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ Mini Blog Application/Models/Profile.cs
@@ -0,0 +1,16 @@
+﻿using MiniBlogApplication.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mini_Blog_Application.Models
+{
+    [Table("Profiles")]
+    public class Profile
+    {
+        
+        public string UserId { get; set; }
+        public string BlogId { get; set; }
+        public User User { get; set; }
+        public BlogPost BlogPost { get; set; }
+    }
+}
